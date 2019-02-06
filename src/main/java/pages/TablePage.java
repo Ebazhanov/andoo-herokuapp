6958bd6ffb24890dda85b5f0570d2ac8ec9caef9
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-06 14:22:14 +0100

    clean up a bit

--- src/main/java/pages/TablePage.java
+++ src/main/java/pages/TablePage.java
@@ -1,29 +1,21 @@
 package pages;
 
-import edu.emory.mathcs.backport.java.util.Collections;
 import io.qameta.allure.Step;
 
 import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static jdk.nashorn.internal.objects.Global.println;
 import static org.testng.Assert.assertEquals;
 
 public class TablePage {
 
-    private static final String FIRST_NAME_HEADER_COLUMN = "#table2 tr th:nth-child(2)";
     public static final String TABLE_2_FIRST_NAME_COLUMN = "#table2 > tbody .first-name";
+    private static final String FIRST_NAME_HEADER_COLUMN = "#table2 tr th:nth-child(2)";
 
     @Step
     public TablePage openTablePage(String tableUrl) {
         open(tableUrl);
-        return this;
-    }
-
-    @Step
-    public TablePage sortResultsByAsceding(List<String> firstNameColumnExpectedSort) {
-
         return this;
     }
 
@@ -37,12 +29,6 @@ public class TablePage {
     public TablePage assertResultHaveTheSameSorting(List<String> firstNameColumnExpectedSort,
                                                     List<String> firstNameColumnActualSort) {
         assertEquals(firstNameColumnExpectedSort, firstNameColumnActualSort);
-        return this;
-    }
-
-    @Step
-    public TablePage sortResultsByDescending(List<String> firstNameColumnExpectedSort) {
-
         return this;
     }
 
